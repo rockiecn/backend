c85ce611d120525f184434cd9c15222c08014997
wy <wy@example.com>
2022-09-09 16:12:21 +0800

    first demo

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"math/big"
@@ -12,23 +13,26 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 
-	"github.com/rockiecn/backend/contracts/erc" // for demo
+	"github.com/rockiecn/backend/contracts/role"
 )
 
 func main() {
+
+	// dial block chain
 	client, err := ethclient.Dial("https://testchain.metamemo.one:24180")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	contractAddress := common.HexToAddress("0x9Ca0f1A85795ea800Ae3F8ed53e174297fbe4c6b")
+	// role address
+	roleAddress := common.HexToAddress("0xC6fE0358b6d8Cb064a0a7db15dbdb7D80F02bD4E")
 
-	// create a query
+	// create a query with contract address
 	query := ethereum.FilterQuery{
 		FromBlock: big.NewInt(0),
 		ToBlock:   big.NewInt(8228622),
 		Addresses: []common.Address{
-			contractAddress,
+			roleAddress,
 		},
 	}
 
@@ -37,70 +41,82 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	println("logs length:", len(logs))
+	println("logs number of role contract:", len(logs))
 
-	// get contract abi
-	contractAbi, err := abi.JSON(strings.NewReader(string(erc.ERC20ABI)))
+	// get role contract abi
+	roleABI, err := abi.JSON(strings.NewReader(string(role.RoleABI)))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	totalIssue := new(big.Int)
+	// event struct
+	type eventStruct struct {
+		topics []interface{}
+		data   []interface{}
+	}
 
-	// tranvel each log
-	fmt.Println("Travelling logs")
+	// create map structure
+	reAccMap := make(map[common.Hash][]eventStruct)
+
+	// scan each log
+	fmt.Println("Travelling logs..")
 	for i, vLog := range logs {
-		// fmt.Println("block hash:", vLog.BlockHash.Hex()) // 0x3404b8c050aa0aacd0223e91b5c32fee6400f357764771d0684fa7b3f448f1a8
-		// fmt.Println("tx hash:", vLog.TxHash.Hex())       // 0x280201eda63c9ff6f305fcee51d5eb86167fab40ca3108ec784e8652a0e2b1a6
 
-		// event := struct {
-		// 	Key   [32]byte
-		// 	Value [32]byte
-		// }{}
-
-		// fmt.Println(string(event.Addr[:]))  // foo
-		// fmt.Println(string(event.Index[:])) // bar
-
-		// 3 topics in all, the first is the hash of the event
-		topics := make([]string, 3)
-		for i := range vLog.Topics {
-			topics[i] = vLog.Topics[i].Hex()
-		}
-
-		// unpack log data
-		res, err := contractAbi.Unpack("Transfer", vLog.Data)
+		// check log hash, should be ReAcc's hash
+		// log Hash: 0xe22ccfc11bbbb1d3350012248b26ad616e32a8a13eece408dd14f1953fe24752
+		byteHash, err := hex.DecodeString("e22ccfc11bbbb1d3350012248b26ad616e32a8a13eece408dd14f1953fe24752")
 		if err != nil {
 			log.Fatal(err)
 		}
-		// only 1 data in Transfer event
-		//fmt.Println("data:", res[0])
+		var recTopic common.Hash
+		recTopic.SetBytes(byteHash)
 
-		// find the issue event, aggregate the total issue
-		cmp := strings.Compare(topics[1], "0x0000000000000000000000000000000000000000000000000000000000000000")
-		if cmp == 0 {
-			fmt.Println("---- log index:", i)
-			fmt.Println("block number:", vLog.BlockNumber) // 2490757
-			for i := range vLog.Topics {
-				fmt.Printf("topic %d: %s\n", i, topics[i])
-			}
-
-			// one time issue
-			issu, ok := res[0].(*big.Int)
-			if !ok {
-				log.Fatal("res assertion failed")
-			}
-			fmt.Println("issue this time:", issu.String())
-
-			// accumulate
-			totalIssue = totalIssue.Add(totalIssue, issu)
-		} else {
+		// skip other events
+		if vLog.Topics[0] != recTopic {
 			continue
 		}
 
+		fmt.Println("-- log index:", i)
+		fmt.Println("-- block number:", vLog.BlockNumber) // 2490757
+
+		// parse topics, 1 topic in all, the hash of the event
+		fmt.Println("parsing ReAcc event..")
+		topics := make([]string, 1)
+		for t := range vLog.Topics {
+			topics[t] = vLog.Topics[t].Hex()
+			fmt.Printf("topic %d: %s\n", t, topics[t])
+		}
+
+		// parse data
+		fmt.Println("parsing data..")
+		data, err := roleABI.Unpack("ReAcc", vLog.Data)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// address and index of account
+		addr, ok := data[0].(common.Address)
+		if !ok {
+			log.Fatal("addr assertion failed")
+		}
+		fmt.Println("address: ", addr)
+
+		index, ok := data[1].(uint64)
+		if !ok {
+			log.Fatal("index assertion failed")
+		}
+		fmt.Println("index: ", index)
+
+		// record event info into map
+		fmt.Println("recording event..")
+		var newEvent eventStruct
+		newEvent.topics = append(newEvent.topics, topics[0])
+		newEvent.data = append(newEvent.data, data[0])
+		newEvent.data = append(newEvent.data, data[1])
+		reAccMap[recTopic] = append(reAccMap[recTopic], newEvent)
+		fmt.Println("recording event ok")
+
 		fmt.Println()
 	}
-
-	fmt.Println("total issue:", totalIssue)
 
 	// eventSignature := []byte("Transfer(address indexed, address indexed, uint256)")
 	// hash := crypto.Keccak256Hash(eventSignature)
